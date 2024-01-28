85e138c57c0fde47363609e4296ca456a9ac9696
Joe <131233284+joe20021313@users.noreply.github.com>
2024-01-28 14:20:50 +0000

    joe
    asdasd

--- option-11/resources/js/Components/NavBar.jsx
+++ option-11/resources/js/Components/NavBar.jsx
@@ -2,15 +2,43 @@ import { Container, Nav, Navbar, Image, NavDropdown } from "react-bootstrap";
 import krakenLogo from "../../assets/Kraken_logo.png";
 import basketIcon from "../../assets/basket-icon.png";
 
-const NavBar = ({ auth, openModal,baskIcon}) => {
-    let loggedIn = false;
-
+const NavBar = ({ auth, openModal, baskIcon }) => {
+    const itemBasket = () => {
+        if (baskIcon.basketCount > 0) {
+            return (
+                <div
+                    className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                    style={{
+                        width: "1.5rem",
+                        height: "1.5rem",
+                        position: "absolute",
+                        top: "22%",
+                        right: "22.5%",
+                    }}
+                >
+                    <span style={{ color: "#fff", fontSize: "1.2rem" }}>
+                        {baskIcon.basketCount}
+                    </span>
+                </div>
+            );
+        }
+    };
     return (
-        <Navbar className="navbar" collapseOnSelect expand="lg" data-bs-theme="dark">
+        <Navbar
+            className="navbar"
+            collapseOnSelect
+            expand="lg"
+            data-bs-theme="dark"
+        >
             <Container>
                 <Navbar.Brand className="nav-logo fs-1" href="/">
                     Option 11
-                    <Image src={krakenLogo} rounded fluid className="kraken-logo" />
+                    <Image
+                        src={krakenLogo}
+                        rounded
+                        fluid
+                        className="kraken-logo"
+                    />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
@@ -18,36 +46,44 @@ const NavBar = ({ auth, openModal,baskIcon}) => {
                         {/* Empty space between the Navbar*/}
                     </Nav>
                     <Nav className="gap-5 nav-links fs-4">
-                        <NavDropdown title="Products" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href={route('products')}>Bikes</NavDropdown.Item>
-                            <NavDropdown.Item href={route('clothing')}>Clothing</NavDropdown.Item>
-                            <NavDropdown.Item href={route('accessoryProducts')}>
+                        <NavDropdown
+                            title="Products"
+                            id="collasible-nav-dropdown"
+                        >
+                            <NavDropdown.Item href={route("products")}>
+                                Bikes
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href={route("clothing")}>
+                                Clothing
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href={route("accessoryProducts")}>
                                 Accessories
                             </NavDropdown.Item>
-                            <NavDropdown.Item href={route('repairKits')}>Repair Kits</NavDropdown.Item>
-                            <NavDropdown.Item href={route('BikeParts')}>Bike Parts</NavDropdown.Item>
-                          
+                            <NavDropdown.Item href={route("repairKits")}>
+                                Repair Kits
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href={route("BikeParts")}>
+                                Bike Parts
+                            </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link
-                            className="text-grey  "
-                            href="/contactUs"
-                        >
+                        <Nav.Link className="text-grey  " href="/contactUs">
                             Contact us
                         </Nav.Link>
-                        <Nav.Link
-                            className="text-grey  "
-                            href="/aboutus"
-                        >
+                        <Nav.Link className="text-grey  " href="/aboutus">
                             About us
                         </Nav.Link>
                         {auth.user ? (
                             <>
-                                 <Nav.Link className="px-4" href="/basket">
-                                    <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{width:"1.5rem", height:"1.5rem", position: "absolute", top: "22%", right: "22.5%", }}><span style={{ color: "#fff", fontSize: "1.2rem" }}>{baskIcon.basketCount}</span></div>
-                                
-                                    <Image src={basketIcon} rounded fluid className="basket-icon" />
+                                <Nav.Link className="px-4" href="/basket">
+                                    {itemBasket()}
+                                    <Image
+                                        src={basketIcon}
+                                        rounded
+                                        fluid
+                                        className="basket-icon"
+                                    />
                                 </Nav.Link>
-                               
+
                                 <Nav.Link
                                     className="text-black bg-info rounded-2 px-4 "
                                     href="/dashboard"
@@ -58,7 +94,6 @@ const NavBar = ({ auth, openModal,baskIcon}) => {
                         ) : (
                             <Nav.Link
                                 className="text-black bg-info rounded-2 px-4 "
-                                
                                 onClick={openModal}
                             >
                                 Login
